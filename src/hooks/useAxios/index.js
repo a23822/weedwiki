63b86bf5a9e3c0b3da697298dbb50375f676b6d2
chanwoochae <a238220@gmail.com>
2022-05-21 02:47:00 +0900

    세팅

--- src/hooks/useAxios/index.js
+++ src/hooks/useAxios/index.js
@@ -1,37 +1,21 @@
 import React, {useState, useEffect, useRef} from 'react';
-import defaultAxios from 'axios';
+import ReactDOM from 'react-dom';
+import useAxios from './useAxios';
 
-const useAxios = (opts, axiosInstance = defaultAxios) => {
-    const [state, setState] = useState({
-        loading: true,
-        error: null,
-        data: null
-    })
-    const [trigger, setTrigger] = useState(0);
-    if (!opts.url) {
-        return;
-    }
-    const refetch = () => {
-        setState({
-            ...state,
-            loading: true
-        });
-        setTrigger(Date.now());
-    }
-    useEffect(() => {
-        axiosInstance(opts).then(data => {
-            setState({
-                ...state,
-                loading: false,
-                data
-            })
-        })
-        .catch(error => {
-            setState({...state, loading: false, error});
-        });
-    }, [trigger])
+const App = () => {
+    const {loading, data, error, refetch } = useAxios({
+        url:"https://yts.am/api/v2/list_movies.json"
+    });
 
-    return {...state, refetch};
-}
+    console.log(`${loading}, ${error}, ${JSON.stringify(data)}`);
+    return (
+        <div className="App" style={{height: "1000vh"}}>
+            <h1>{data && data.status}</h1>
+            <h2>{loading?"Loading":"finish"}</h2>
+            <button onClick={refetch}>Refetch</button>
+        </div>
+    );
+};
 
-export default useAxios;
+const rootElement = document.getElementById("root");
+ReactDOM.render(<App />, rootElement);
