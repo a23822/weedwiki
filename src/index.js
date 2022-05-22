2991ff5e3c49ee06a187bbe07ce6f2ce322489c8
chanwoochae <a238220@gmail.com>
2022-05-22 10:09:50 +0900

    firebase 연동

--- src/index.js
+++ src/index.js
@@ -1,7 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import firebaseApp from './firebase';
 import reportWebVitals from './reportWebVitals';
+
+console.log(firebaseApp);
 
 ReactDOM.render(
   <React.StrictMode>
